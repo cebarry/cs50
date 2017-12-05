8200988f6922b24e4645d580db437c8cd319ced9
Ce Barry <cecebarry4@hotmail.com>
2017-12-05 22:22:01 +0800

    Create hello.c

---
+++ pset1/hello.c
@@ -0,0 +1,14 @@
+/*
+    hello.c
+
+    Author : Cecilia Flora Barry  - cecebarry4@hotmail.com
+
+    This file is for  pset1 of CS50 - hello
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello, world\n");
+}
