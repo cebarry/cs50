7c8eb345f64a8223896469cd301b65c4fa37d4e1
Ce Barry <cecebarry4@hotmail.com>
2017-12-05 22:23:28 +0800

    Create water.c

---
+++ pset1/water.c
@@ -0,0 +1,28 @@
+/*
+    water.c
+
+    Author : Cecilia Flora Barry  - cecebarry4@hotmail.com
+
+    This file is for  pset1 of CS50 - water.
+*/
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int minutes, bottles;
+
+    do //  user input length of shower in minutes
+    {
+        printf("Minutes : ");
+        minutes = get_int();
+
+    if (minutes > 0) //  calculations
+        {
+            bottles = (minutes * 12);
+            printf("Bottles : %d\n", bottles);
+        }
+    }
+    while (minutes <= 0);
+}
