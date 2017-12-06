c15d4ffa37fc684d18f8772268014dc8877dea8e
Ce Barry <cecebarry4@hotmail.com>
2017-12-06 12:30:02 +0800

    Update hello.c

--- pset1/hello.c
+++ pset1/hello.c
@@ -4,6 +4,10 @@
     Author : Ce Barry  - cecebarry4@hotmail.com
 
     This file is for  pset1 of CS50 - hello
+    
+    Implement a program that prints out a simple greeting to the user, per the below.
+    $ ./hello
+    hello, world
 */
 
 #include <stdio.h>
